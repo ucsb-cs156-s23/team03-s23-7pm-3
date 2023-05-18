281ceef3c6d8b938431ed605d2191b0655a05f7b
khtran1 <khtran@ucsb.edu>
2023-05-18 14:31:15 -0700

    kt - add pages and tests; tests passing

---
+++ frontend/src/main/pages/IceCreamShops/IceCreamShopCreatePage.js
@@ -0,0 +1,24 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import IceCreamShopForm from "main/components/IceCreamShops/IceCreamShopForm";
+import { useNavigate } from 'react-router-dom'
+import { iceCreamShopUtils } from 'main/utils/iceCreamShopUtils';
+
+export default function IceCreamShopCreatePage() {
+
+  let navigate = useNavigate(); 
+
+  const onSubmit = async (iceCreamShop) => {
+    const createdIceCreamShop = iceCreamShopUtils.add(iceCreamShop);
+    console.log("createdIceCreamShop: " + JSON.stringify(createdIceCreamShop));
+    navigate("/iceCreamShops");
+  }  
+
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>Create New Ice Cream Shop</h1>
+        <IceCreamShopForm submitAction={onSubmit} />
+      </div>
+    </BasicLayout>
+  )
+}
---
+++ frontend/src/tests/pages/IceCreamShops/IceCreamShopCreatePage.test.js
@@ -0,0 +1,92 @@
+import { render, screen, fireEvent, act, waitFor } from "@testing-library/react";
+import IceCreamShopCreatePage from "main/pages/IceCreamShops/IceCreamShopCreatePage";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import mockConsole from "jest-mock-console";
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate
+}));
+
+const mockAdd = jest.fn();
+jest.mock('main/utils/iceCreamShopUtils', () => {
+    return {
+        __esModule: true,
+        iceCreamShopUtils: {
+            add: () => { return mockAdd(); }
+        }
+    }
+});
+
+describe("IceCreamShopCreatePage tests", () => {
+
+    const queryClient = new QueryClient();
+    test("renders without crashing", () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <IceCreamShopCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("redirects to /iceCreamShops on submit", async () => {
+
+        const restoreConsole = mockConsole();
+
+        mockAdd.mockReturnValue({
+            "iceCreamShop": {
+                id: 3,
+                name: "Baskin Robbins",
+                description: "A lot of ice cream",
+                flavor: "Vanilla"
+            }
+        });
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <IceCreamShopCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        )
+
+        const nameInput = screen.getByLabelText("Name");
+        expect(nameInput).toBeInTheDocument();
+
+
+        const descriptionInput = screen.getByLabelText("Description");
+        expect(descriptionInput).toBeInTheDocument();
+
+        const flavorInput = screen.getByLabelText("Most Popular Flavor");
+        expect(flavorInput).toBeInTheDocument();
+
+        const createButton = screen.getByText("Create");
+        expect(createButton).toBeInTheDocument();
+
+        await act(async () => {
+            fireEvent.change(nameInput, { target: { value: 'Baskin Robbins' } })
+            fireEvent.change(descriptionInput, { target: { value: 'A lot of ice cream' } })
+            fireEvent.change(flavorInput, { target: { value: 'Vanilla' } })
+
+            fireEvent.click(createButton);
+        });
+
+        await waitFor(() => expect(mockAdd).toHaveBeenCalled());
+        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith("/iceCreamShops"));
+
+        // assert - check that the console.log was called with the expected message
+        expect(console.log).toHaveBeenCalled();
+        const message = console.log.mock.calls[0][0];
+        const expectedMessage =  `createdIceCreamShop: {"iceCreamShop":{"id":3,"name":"Baskin Robbins","description":"A lot of ice cream","flavor":"Vanilla"}`
+
+        expect(message).toMatch(expectedMessage);
+        restoreConsole();
+
+    });
+
+});
+
