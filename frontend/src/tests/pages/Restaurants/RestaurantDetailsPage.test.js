82e64fa38a6eeb881a2e061f7e5505eec85a15eb
KaneD985 <ydeng672@ucsb.edu>
2023-05-20 00:26:43 -0700

    Kane-Fix the axios

--- frontend/src/tests/pages/Restaurants/RestaurantDetailsPage.test.js
+++ frontend/src/tests/pages/Restaurants/RestaurantDetailsPage.test.js
@@ -2,6 +2,10 @@ import { render, screen } from "@testing-library/react";
 import RestaurantDetailsPage from "main/pages/Restaurants/RestaurantDetailsPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
+import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
 
 const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -34,7 +38,7 @@ describe("RestaurantDetailsPage tests", () => {
     const axiosMock =new AxiosMockAdapter(axios);
     axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
     axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither); 
-    
+
     const queryClient = new QueryClient();
     test("renders without crashing", () => {
         render(
--- frontend/src/tests/pages/Restaurants/RestaurantEditPage.test.js
+++ frontend/src/tests/pages/Restaurants/RestaurantEditPage.test.js
@@ -3,6 +3,10 @@ import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import mockConsole from "jest-mock-console";
+import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
 
 const mockNavigate = jest.fn();
 
@@ -39,7 +43,7 @@ describe("RestaurantEditPage tests", () => {
     const axiosMock =new AxiosMockAdapter(axios);
     axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
     axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither); 
-    
+
     const queryClient = new QueryClient();
 
     test("renders without crashing", () => {
